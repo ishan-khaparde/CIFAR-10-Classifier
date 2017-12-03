9d0c92c2b463ee8d9417ca481a5f93601cc0c158
Ishan Khaparde <ishankhaparde@gmail.com>
2017-12-03 14:54:54 -0500

    Removing methods for building the CNN

--- BuildNN.py
+++ BuildNN.py
@@ -1,7 +1,7 @@
 import tensorflow as tf 
 import numpy as np 
 from tf import layers
-from tf.examples.tutorials.cifar10 import cifar10
+from tensorflow.models.image.cifar10 import cifar10
 
 #hyperparameters
 learning_rate = 0.001
@@ -14,37 +14,14 @@ input_size = 1024
 num_classes = 10
 dropout = 0.75
 
-x = tf.placeholder(tf.float32, [None,input_size])
-y = tf.placeholder(tf.float32,[None,num_classes])
-keep_prob = tf.placeholder(tf.float32)
+#Input Layer
+input_layer = tf.layers.Input(shape=[32,32,3],batch_size=batch_size)
 
-def conv2d(x,W,b,strides = 1):
-    x = tf.nn.conv2d(x,W,strides=[1,strides,strides,1] padding='SAME')
-    x = tf.nn.bias_add(x,b)
-    return tf.nn.relu(x)
+#Convolution Layer 1
+conv2d_1 = tf.layers.conv2d(inputs=input_layer,filters = 32, kernel_size=[3,3],padding = 'same',activation=tf.nn.relu)
 
-def maxpool2d(x,k=2):
-    return tf.nn.max_pool(x,ksize=[1,k,k,1],strides=[1,k,k,1],padding='SAME')
+#Pooling Layer
+pool2d_1 = tf.layers.max_pooling2d(inputs=conv2d_1,pool_size=[2,2],strides=2)
 
-def conv_net(x,weights,biases,dropout):
-    x = tf.reshape(x,shape=[-1,32,32,3])
-
-    conv_1 = conv2d(x,weights['wc1'],biases['bc1'])
-
-    conv_1 = maxpool2d(conv_1,k = 2)
-
-    conv_2 = conv2d(conv_1,weights['wc2'], biases['bc2'])
-
-    conv_2 = maxpool2d(conv_2,k=2)
-
-    conv_3 = conv2d(conv_2,weights['wc3',biases['bc3']])
-
-    conv_3 = maxpool2d(conv_3,k=2)
-
-    fc1 = tf.reshape(conv_3, [-1, weights['wd1'].get_shape().as_list()[0]])
-    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
-    fc1 = tf.nn.relu(fc1)
-
-    fc1 = tf.nn.dropout(fc1,dropout)
-
-    return tf.add(tf.matmul(fc1,weights['out'],biases['out']))
+#Convolution Layer 2
+conv2d_2 = tf.layers.conv2d(inputs = pool2d_1,filters = 64, kernel_size = [3,3],padding='same',activation=tf.nn.relu)
